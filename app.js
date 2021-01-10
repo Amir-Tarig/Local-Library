3f0d6bcf8c055fe12b97a98e7fb6533e4a9c28f9
Miro <MiroPhoenix91@gmail.com>
2021-01-10 12:25:24 +0100

    mongoose schema

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var soCool = require('./routes/cool');
 
 var app = express();
 
+//setting up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = "mongodb+srv://miro:miro91@cluster0.nzkmt.mongodb.net/local_library?retryWrites=true&w=majority";
+mongoose.connect(mongoDB,{ useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
